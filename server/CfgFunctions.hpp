1515afc86d4de95d154756b5ef762c765d2c61e9
Ivan <ivan_90.90@mail.ru>
2014-12-18 20:58:44 +0300

    Supply trucks returned! Yay!

--- server/CfgFunctions.hpp
+++ server/CfgFunctions.hpp
@@ -7,7 +7,8 @@
 class ServerDir
 {
 	class generateKey {file = "server\functions\network\generateKey.sqf";};
-	class setupStoreNPC {file = "server\functions\setupStoreNPC.sqf";};
 	class setupMissionObject {file = "server\functions\setupMissionObject.sqf";};
-    class setupSellTruck {file = "server\functions\setupSellTruck.sqf";};
+	class setupResupplyTruck {file = "server\functions\setupResupplyTruck.sqf";};
+	class setupSellTruck {file = "server\functions\setupSellTruck.sqf";};
+	class setupStoreNPC {file = "server\functions\setupStoreNPC.sqf";};
 };
